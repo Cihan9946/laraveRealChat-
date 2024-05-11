5e11ede6f3edb35b97dc565a3eb7e0bd62ceb7e2
Mustafa Cihan İncir <113993331+Cihan9946@users.noreply.github.com>
2024-05-11 22:20:29 +0300

    Update app.blade.php

--- resources/views/panel/app.blade.php
+++ resources/views/panel/app.blade.php
@@ -19,6 +19,7 @@
     <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <title>
+        
         Fırat Sohbet
     </title>
 
